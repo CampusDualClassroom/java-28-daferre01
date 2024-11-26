349b90b28f3e061dc34edfc2a699eff4513afb7d
David Fernandez Reboredo <davidfr2001@hotmail.com>
2024-11-26 17:13:54 +0100

    Ejercicio hecho

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,8 @@
 package com.campusdual.classroom;
 
 public class Exercise28 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)throws Exception {
+            throw new Exception("Esta es una excepcion");
+//            System.out.println(numeros[3]);
     }
 }
